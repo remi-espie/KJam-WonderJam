76b56246482b6261d7dc27d256d8ba2641e1c60e
Rémi Espié <rem-e.84@orange.fr>
2021-10-08 22:56:09 -0400

    starting button prototype

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            transform.Translate(0f, 1f, 0f);  
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            transform.Translate(0f, -1f, 0f);  
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.Translate(1f, 0f, 0f);  
+        }
+        if (Input.GetKeyDown("q"))
+        {
+            transform.Translate(-1f, 0f, 0f);  
+        }
+    }
+}
