fd640f4114efd84c3aff532ce0e3c332c3041be4
Rémi Espié <rem-e.84@orange.fr>
2021-10-09 12:27:11 -0400

    prefab key door

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public SpriteRenderer key;
+    public SpriteRenderer door;
+    public Sprite openDoor;
+    private void OnTriggerEnter2D(Collider2D collider2D)
+    {
+            door.sprite = openDoor;
+            door.GetComponent<BoxCollider2D>().enabled = false;
+            Destroy(gameObject);
+    }   
+}
