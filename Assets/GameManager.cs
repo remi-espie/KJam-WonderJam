fc6fcd112be6a3a5385694c0384a24a47a511631
AlexandreBuzon <buzon130@dartybox.com>
2021-10-09 04:39:21 +0200

    GameManager start

---
+++ Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject camera;
+    [SerializeField] List<transform> tab;
+    float smoothSpeed = 0.125f;
+    private bool Player1End { get; }
+    private bool Player2End { get; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player1End = false;
+        Player2End = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player1End && Player2End)
+        {
+            
+        }
+    }
+
+
+}
