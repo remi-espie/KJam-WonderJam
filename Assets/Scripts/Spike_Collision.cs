62687f4aced60fd178fbfaa22138cc29d150f855
Arthur 'RainbowSuperLoki' Sapin <rainbowsuperloki@gmail.com>
2021-10-09 11:52:40 -0400

    Fixed sprite renderings

---
+++ Assets/Scripts/Spike_Collision.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Spike_Collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider2D)
+    {
+
+        if (collider2D.tag == "Player" || collider2D.tag == "Player1" || collider2D.tag == "Player2")
+        {
+            SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+
+        }
+    }
+}
