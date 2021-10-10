ee6d4389d3fc2003670ddec95ebef98fc2667db1
AlexandreBuzon <buzon130@dartybox.com>
2021-10-10 20:48:08 +0200

    UI GameOver

---
+++ Assets/Scripts/GameOver buttons.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverbuttons : MonoBehaviour
+{
+    [SerializeField] Animator transition;
+    [SerializeField] float transitionTime = 1.5f;
+
+    public void Restart()
+    {
+        LoadNextScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void LevelSelection()
+    {
+        LoadNextScene("LevelSelection");
+    }
+
+    public void LoadNextScene(string nextLevel)
+    {
+        StartCoroutine(LoadLevel(nextLevel));
+    }
+
+    IEnumerator LoadLevel(string nextLevel)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(nextLevel);
+    }
+
+}
