70364b61064bd0657e2d716b97f06c5da8254130
AlexandreBuzon <buzon130@dartybox.com>
2021-10-10 18:46:30 +0200

    Transition trop cool

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,40 +5,130 @@ using UnityEngine.SceneManagement;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
-    private static GameManager instance = new GameManager();
+    private static GameManager instance = null;
 
     private int currentLvl { get; set; }
     private int maxLvlRich { get; set; }
-    private int[] collectable { get; set; }
+    //private int[] collectable { get; set; }
     private int[] nbTries { get; set; }
+    private int[] bestNbTries { get; set; }
 
-    void Start()
+    void Awake()
     {
-        //LoadPlayer();
+        if (!Instance)
+        {
+            Load();
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
-    public void SavePlayer()
+
+    /*public void SavePlayer()
     {
         SaveSysteme.SavePlayer(this);
+    }*/
+
+    public void resetForNextLvl()
+    {
+        if(bestNbTries[currentLvl] > nbTries[currentLvl])
+            bestNbTries[currentLvl] = nbTries[currentLvl];
+        
+        currentLvl++;
+        
+        if(maxLvlRich < currentLvl)
+            MaxLvlRich = currentLvl;
+        
+        nbTries[currentLvl] = 0;
+        
+        Save();
     }
 
-    public void LoadPlayer()
+    /*public void LoadPlayer()
     {
         PlayerData data = SaveSysteme.LoadPlayer();
 
         currentLvl = data.CurrentLvl;
         maxLvlRich = data.MaxLvlRich;
-        collectable = data.Collectable;
+        //collectable = data.Collectable;
         nbTries = data.NbTries;
-    }
+        Debug.Log(data.ToString());
+    }*/
 
     private GameManager()
     {
-        LoadPlayer();
+        currentLvl = 0;
+        maxLvlRich = 1;
+        //collectable[0] = 0;
+        nbTries = new int[4];
+        for(int i = 0; i < nbTries.Length; i++)
+        {
+            nbTries[i] = 0;
+        }
+        bestNbTries = new int[4];
+        for (int i = 0; i < bestNbTries.Length; i++)
+        {
+            bestNbTries[i] = 0;
+        }
+    }
+    private string SaveFilePath
+    {
+        get { return Application.persistentDataPath + "/playerInfo.sa"; }
     }
 
-    public static GameManager Instance
+    public void DeleteSave()
+    {
+        try
+        {
+            File.Delete(SaveFilePath);
+        }
+        catch (IOException ex)
+        {
+            Debug.LogException(ex);
+        }
+    }
+        public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream stream = new FileStream(Application.persistentDataPath + "/playerInfo.sa", FileMode.Create);
+
+        PlayerData data = new PlayerData(this);
+        data.CurrentLvl = GameManager.Instance.currentLvl;
+        data.MaxLvlRich = GameManager.Instance.maxLvlRich;
+        data.NbTries = GameManager.Instance.nbTries;
+        data.BestNbTries = GameManager.Instance.bestNbTries;
+
+        bf.Serialize(stream, data);
+        stream.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.sa"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream stream = new FileStream(Application.persistentDataPath + "/playerInfo.sa", FileMode.Open);
+            PlayerData data = (PlayerData)bf.Deserialize(stream);
+
+            GameManager.instance.currentLvl = data.CurrentLvl;
+            GameManager.instance.maxLvlRich = data.MaxLvlRich;
+            GameManager.instance.nbTries = data.NbTries;
+            GameManager.instance.bestNbTries = data.BestNbTries;
+
+            stream.Close();
+        }
+        else
+        {
+            Debug.Log("File does not exist.");
+        }
+    }
+
+public static GameManager Instance
     {
         get
         {
@@ -60,15 +150,20 @@ public class GameManager
         get { return maxLvlRich; }
         set { maxLvlRich = value; }
     }
-    public int[] Collectable
+    /*public int[] Collectable
     {
         get { return collectable; }
         set { collectable = value; }
-    }
+    }*/
     public int[] NbTries
     {
         get { return nbTries; }
         set { nbTries = value; }
+    }
+    public int[] BestNbTries
+    {
+        get { return bestNbTries; }
+        set { bestNbTries = value; }
     }
 
 }
--- Assets/Scripts/CameraControllerSelectLevel.cs
+++ Assets/Scripts/CameraControllerSelectLevel.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using UnityEngine.UI;
+using TMPro;
 
 public class CameraControllerSelectLevel : MonoBehaviour
 {
     [SerializeField] List<GameObject> tab;
     [SerializeField] List<GameObject> stairs;
     [SerializeField] List<GameObject> Lock;
-    public GameObject gameManager;
+    [SerializeField] List<GameObject> nbTriesText;
+    [SerializeField] List<Sprite> BouttonsAscensseur;
+    [SerializeField] SpriteRenderer SpriteButton;
+    [SerializeField] Animator transition;
+    [SerializeField] float transitionTime = 1f;
+    //public GameObject gameManager;
     [SerializeField] Vector3 offset;
 
     int index = 0;
@@ -21,18 +28,25 @@ public class CameraControllerSelectLevel : MonoBehaviour
     private void Start()
     {
         tab[index].GetComponent<Animator>().SetBool("isSelected", true);
-        Debug.Log(GameManager.Instance.MaxLvlRich);
+        Debug.Log("MaxLvlRich "+GameManager.Instance.MaxLvlRich);
+        Debug.Log("CurrentLvl "+GameManager.Instance.CurrentLvl);
+        //Debug.Log("Nbtries "+GameManager.Instance.NbTries);
         int nbLevel = GameManager.Instance.MaxLvlRich;
         int nbStairs = GameManager.Instance.MaxLvlRich - 1;
+        for(int i = 0; i < tab.Count; i++)
+        {
+            int[] nbTriesLvl = GameManager.Instance.BestNbTries;
+            nbTriesText[i].GetComponent<TextMeshPro>().text = nbTriesLvl[i].ToString()+" Tries";
+        }
         for (int i = 0; i < nbStairs; i++)
         {
             stairs[i].gameObject.SetActive(true);
         }
-        for(int i=0; i < nbLevel-1; i++)
+        for(int i=0; i < nbLevel; i++)
         {
             Lock[i].gameObject.SetActive(false);
+            nbTriesText[i].gameObject.SetActive(true);
         }
-        DontDestroyOnLoad(gameManager);
     }
 
     // Update is called once per frame
@@ -40,24 +54,57 @@ public class CameraControllerSelectLevel : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            if(index+1 < tab.Count-1 && index+1 <= GameManager.Instance.MaxLvlRich)
+            if(index+1 < tab.Count-1 && index+1 < GameManager.Instance.MaxLvlRich)
+            {
                 NextPos();
+                SpriteButton.sprite = BouttonsAscensseur[1];
+            }
+                
+        }
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            SpriteButton.sprite = BouttonsAscensseur[0];
         }
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            if (index - 1 >= 0 )
+            if (index - 1 >= 0)
+            {
                 PreviousPos();
+                SpriteButton.sprite = BouttonsAscensseur[2];
+            }
+            
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            SpriteButton.sprite = BouttonsAscensseur[0];
         }
         if (Input.GetKeyDown(KeyCode.KeypadEnter))
         {
             //save data player
-
+            GameManager.Instance.CurrentLvl++;
+            GameManager.Instance.NbTries[GameManager.Instance.CurrentLvl] = 0;
+            //GameManager.Instance.Collectable[GameManager.Instance.CurrentLvl] = 0;
             int numLvl = index + 1;
-            string levelSelected = "Level" + numLvl;//to remove
+            string levelSelected = "AlexLevel" + numLvl+"Test";//to remove
             SceneManager.LoadScene(levelSelected);
         }
 
     }
+
+    public void LoadNextScene(string nextLevel)
+    {
+        StartCoroutine(LoadLevel(nextLevel));
+    }
+
+    IEnumerator LoadLevel(string nextLevel)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(nextLevel);
+    }
+
 
     private void FixedUpdate()
     {
@@ -74,14 +121,13 @@ public class CameraControllerSelectLevel : MonoBehaviour
         if (index > tab.Count - 1)
         {
             index = tab.Count - 1;
-            //Debug.Log("Change scene");
         }
         else
         {
             desiredPosition = tab[index].transform.position + offset;
             desiredPosition.x = 0;
             tab[index].GetComponent<Animator>().SetBool("isSelected", true);
-            Debug.Log(desiredPosition);
+            //Debug.Log(desiredPosition);
             camera_move_enabled = true;
         }
 
@@ -101,7 +147,7 @@ public class CameraControllerSelectLevel : MonoBehaviour
             desiredPosition = tab[index].transform.position + offset;
             desiredPosition.x = 0;
             tab[index].GetComponent<Animator>().SetBool("isSelected", true);
-            Debug.Log(desiredPosition);
+            //Debug.Log(desiredPosition);
             camera_move_enabled = true;
         }
 
