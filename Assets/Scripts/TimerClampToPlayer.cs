ec07da51f41bdb7c107a486a326b2738398df1fb
Alex BH <kenpachi.zaken@gmail.com>
2021-10-10 11:30:45 -0400

    Auto stash before merge of "feature/level_two" and "origin/develop"
    Merged dev.
    Started level two.
    Trying to clamp timer to player.

---
+++ Assets/Scripts/TimerClampToPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerClampToPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private Text timerLabel;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float xPos = this.transform.position
+        Vector2 timerPos = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
+        timerLabel.transform.position = timerPos;
+    }
+}
